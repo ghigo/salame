483da27313940c6e78903ff80ddc717f337037d7
Marco Sgrignuoli <marco.sgrignuoli@gmail.com>
2015-05-15 02:06:05 +0200

    Launcher

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-until sudo python main.py; do
+
+# https://stackoverflow.com/questions/696839/how-do-i-write-a-bash-script-to-restart-a-process-if-it-dies/697064#697064
+# Remember to: chmod 755 launcher.sh
+#
+# Add to chronjob:
+# crontab -e
+# Add line:
+# @reboot sh /home/pi/salame/launcher.sh >/home/pi/logs/cronlog.log 2>&1
+
+until sudo python /home/pi/salame/main.py; do
   echo "Salame 'main.py' crashed with exit code $?. Restarting..." >&2
   sleep 1
 done
