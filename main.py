b33e6c8ac5a7d6a0d50e5be2e0fa1118c1134641
Marco Sgrignuoli <marco.sgrignuoli@gmail.com>
2015-05-20 03:33:56 +0200

    Add relays fridge and humidifier

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import sys
 
 from google_logger import Google_spreadsheet
 from led import Led
+from relay import Relay
 from switch import Switch
 from temphumid import Temphumid
 
@@ -52,7 +53,11 @@ class Salame(object):
     self.led1 = Led(11, 'LED-yellow-1')
     self.led2 = Led(15, 'LED-yellow-2')
     self.th_sensor = Temphumid(4)
+    self.fridge = Relay(12, 'Fridge')
+    self.humidifier = Relay(16, 'Humidifier')
     self.data_logger = Google_spreadsheet(GDOCS_SPREADSHEET_NAME)
+
+    self.tmp_test_relay()
 
     # Create threads
     self.alert_started_worker = threading.Thread(target=self.alert_started)
@@ -99,6 +104,17 @@ class Salame(object):
     self.sample_switch.off()
     # print "sample_switch status"
     # print self.sample_switch.get_status()
+
+  def tmp_test_relay(self):
+    self.relay1 = Relay(18, 'relay-1')
+    # self.switch2.off()
+    for x in range(0, 3):
+      self.relay1.on()
+      time.sleep(2)
+      self.relay1.off()
+      time.sleep(2)
+    status = self.relay1.get_status()
+    print ('relay status:', status)
 
 
 # init app
